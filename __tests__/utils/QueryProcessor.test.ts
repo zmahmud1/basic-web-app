c0023d53b53a9bfa0d99b8fb21bcf6fd1a0fe6c0
zmahmud <zmahmud@andrew.cmu.edu>
2025-10-06 14:28:23 -0400

    Added Andrew ID Query

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -18,6 +18,14 @@ describe("QueryProcessor", () => {
           ));
     });
 
+    test('should return andrew description', () => {
+        const query = "andrew";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "zmahmud"
+          ));
+    });
+
     test('should return name', () => {
         const query = "What is your name?";
         const response: string = QueryProcessor(query);
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,6 +7,12 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrew")) {
+    return (
+      "zmahmud"
+    );
+  }
+
   if (query.toLowerCase().includes("name")) {
     return "Rohan";
   }
